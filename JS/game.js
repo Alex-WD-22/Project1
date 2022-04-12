bbaa6e4f100e3fa02a269d05bf0652fc0a30fe69
Alexander <alexander.nerz@web.de>
2022-04-12 21:30:32 +0200

    soulution in random places fixed + li added

--- JS/game.js
+++ JS/game.js
@@ -1,5 +1,11 @@
+document.getElementById("btn-start").addEventListener("click", startGame);
 const equation = document.getElementById("random-equation")
- let checkAnswer = {
+const restartbtn = document.getElementById('restart').disabled = true;
+let totalTime = null;
+let timer = 8;
+
+
+let checkAnswer = {
     wrongAnswers: 0,
     score: 0
 }
@@ -7,83 +13,125 @@ const equation = document.getElementById("random-equation")
 function problems() {
     checkAnswer.newProblem = toCalculate();
     equation.innerHTML = `${checkAnswer.newProblem.numb1} ${checkAnswer.newProblem.operator} ${checkAnswer.newProblem.numb2}`
-}
-problems();
 
-function randomNumber (number) {
+}
+
+
+function randomNumber(number) {
     return Math.floor(Math.random() * (number + 1));
 }
 
 function toCalculate() {
-    return  {
+    return {
         numb1: randomNumber(14),
         numb2: randomNumber(14),
-        operator: ['-','+','x','÷'] [randomNumber(2)]
+        operator: ['+', '-', 'x'][randomNumber(2)]
     }
 }
-document.getElementById("btn-start").addEventListener("click", setTimer);
-let totalTime;
-let timer = 8;
-function setTimer() {
 
-    document.getElementById("btn-start").disabled = true;    
+function startGame() {
+    shuffle();
+    setTimer();
+    problems();
+    answerbtn();
+    correctAnswer();
     toCalculate();
-    let countdown = document.getElementById("timer")
-    totalTime = setInterval(function() {
-    timer -= 1;
-    countdown.innerText ="Time Left:" + timer;
-    if (timer < 0 ) {
-        clearInterval(totalTime);
-        gameOver();
-        document.getElementById('btn-start').disabled = false;
-    } 
-    }, 1000);
-    } 
-
-function gameOver () {
-    alert('Game Over');
 }
 
-function correctAnswer () {
-let rightAnswer
+function gameOver() {
+    document.getElementById('restart').disabled = false;
+    document.getElementById('restart').addEventListener('click', () => {
+        resetGame();
+    }) 
+}
+
+function resetGame() {
+    timer = 8 ;
+    let resetTimer = document.getElementById('timer')
+    resetTimer.innerHTML = timer;
+    startGame();
+}
+
+// function randombtn () {
+//     for (let i = 0; i < array.length; i++)
+//     let i = math.floor(Math.random() * (i + 1));
+
+// }
+
+
+function setTimer() {
+
+    document.getElementById("btn-start").disabled = true;
+    
+    let countdown = document.getElementById("timer")
+    totalTime = setInterval(function () {
+        timer -= 1;
+        countdown.innerText = "Time Left:" + timer;
+        if (timer === 0) {
+            
+            clearInterval(totalTime);
+            gameOver();
+            // countdown.innerText = "Time Left:" + timer;
+            document.getElementById('btn-start').disabled = true;
+        }
+    }, 1000); 
+   
+}
+
+function correctAnswer() {
+    let rightAnswer
     if (checkAnswer.newProblem.operator === '+') {
         rightAnswer = checkAnswer.newProblem.numb1 + checkAnswer.newProblem.numb2
     } else if (checkAnswer.newProblem.operator === '-') {
         rightAnswer = checkAnswer.newProblem.numb1 - checkAnswer.newProblem.numb2
-    } else if (checkAnswer.newProblem.operator === 'x') {
+    } else if (checkAnswer.newProblem.operator === 'x')
         rightAnswer = checkAnswer.newProblem.numb1 * checkAnswer.newProblem.numb2
-    } else if (checkAnswer.newProblem.operator === '÷') {
-        rightAnswer = checkAnswer.newProblem.numb1 / checkAnswer.newProblem.numb2
-    } else if (rightAnswer === `${checkAnswer.newProblem.numb1} ${checkAnswer.newProblem.operator} ${checkAnswer.newProblem.numb2}`) {
-        problems();
-    } else {
-        alert('Wrong answer')
-    }
+
     console.log(rightAnswer);
+
+    const rightAnswerbtn = document.getElementById('btn-answer1')
+    rightAnswerbtn.innerHTML = rightAnswer;
+    rightAnswerbtn.addEventListener('click', startGame)
+
 }
 
-function randomnumb () {
+function shuffle () {
+    let ul = document.querySelector('ul');
+for (let i = ul.children.length; i >= 0; i--) {
+   let randomnumb = ul.appendChild(ul.children[Math.random() * i | 0]);
+    console.log(randomnumb)
+}
+
+}
+
+function randomnumb() {
     let max = 196;
     let min = 0;
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function answerbtn () {
-const wrongAnswerbtn1 = document.getElementById('btn-answer1')
-const wrongAnswerbtn2 = document.getElementById('btn-answer2')
-const wrongAnswerbtn3 = document.getElementById('btn-answer3')
-const wrongAnswerbtn4 = document.getElementById('btn-answer4')
-const wrongAnswerbtn5 = document.getElementById('btn-answer5')
-const wrongAnswerbtn6 = document.getElementById('btn-answer6')
+function answerbtn() {
 
-wrongAnswerbtn1.innerHTML = randomnumb();
-wrongAnswerbtn2.innerHTML = randomnumb();
-wrongAnswerbtn3.innerHTML = randomnumb();
-wrongAnswerbtn4.innerHTML = randomnumb();
-wrongAnswerbtn5.innerHTML = randomnumb();
-wrongAnswerbtn6.innerHTML = randomnumb();
+    const wrongAnswerbtn2 = document.getElementById('btn-answer2')
+    const wrongAnswerbtn3 = document.getElementById('btn-answer3')
+    const wrongAnswerbtn4 = document.getElementById('btn-answer4')
+    const wrongAnswerbtn5 = document.getElementById('btn-answer5')
+    const wrongAnswerbtn6 = document.getElementById('btn-answer6')
+
+    wrongAnswerbtn2.innerHTML = randomnumb();
+    wrongAnswerbtn3.innerHTML = randomnumb();
+    wrongAnswerbtn4.innerHTML = randomnumb();
+    wrongAnswerbtn5.innerHTML = randomnumb();
+    wrongAnswerbtn6.innerHTML = randomnumb();
+
+    wrongAnswerbtn2.addEventListener('click', gameOver)
+    wrongAnswerbtn3.addEventListener('click', gameOver)
+    wrongAnswerbtn4.addEventListener('click', gameOver)
+    wrongAnswerbtn5.addEventListener('click', gameOver)
+    wrongAnswerbtn6.addEventListener('click', gameOver)
 
 }
-answerbtn();
+
+
 
 
